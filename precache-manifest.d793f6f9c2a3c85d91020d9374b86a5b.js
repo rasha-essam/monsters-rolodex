680c4f2ea8b25fc0d7cc0e78fe6eee2c8076b126
rasha-essam <engrasha88@gmail.com>
2020-06-15 04:12:08 +0200

    Updates

---
+++ precache-manifest.d793f6f9c2a3c85d91020d9374b86a5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f717823481956d7a6c5bb2ffd0285d6f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "12981c19e35b8369804c",
+    "url": "/monsters-rolodex/static/css/main.4f6fd437.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "12981c19e35b8369804c",
+    "url": "/monsters-rolodex/static/js/main.715e6e28.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
